77df8a65c56e29a001816173e17d7cea643aa78b
GermanMT <germanoctako@gmail.com>
2023-10-18 01:34:45 +0200

    delete unnused code

--- src/cve.py
+++ src/cve.py
@@ -10,18 +10,6 @@ from pymongo import InsertOne
 
 cve_uri: str = 'https://services.nvd.nist.gov/rest/json/cves/2.0?startIndex='
 
-async def get_products(cve: dict[str, Any]) -> list[str]:
-    products: list[str] = []
-    if 'configurations' in cve:
-        for configuration in cve['configurations']:
-            for node in configuration['nodes']:
-                if 'cpeMatch' in node:
-                    for cpe_match in node['cpeMatch']:
-                        product = cpe_match['criteria'].split(':')[4]
-                        if product not in products:
-                            products.append(product)
-    return products
-
 async def clone_cves(client, delay, headers):
     nvd_clone_db: AsyncIOMotorDatabase = client.nvd
     cves_collection: AsyncIOMotorCollection = nvd_clone_db.get_collection('cves')
@@ -35,10 +23,8 @@ async def clone_cves(client, delay, headers):
                 break
             except:
                 sleep(6)
-        for match_string in response['vulnerabilities']:
-            cve = match_string['cve']
-            cve['products'] = await get_products(cve)
-            actions.append(InsertOne(cve))
+        for vulnerability in response['vulnerabilities']:
+            actions.append(InsertOne(vulnerability['cve']))
         index+=response['resultsPerPage']
         await cves_collection.bulk_write(actions, ordered=False)
         if index == response['totalResults']:
