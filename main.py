41791c39fde2504c911d3e3d7c581df460dd8ff9
GermanMT <germanoctako@gmail.com>
2023-10-21 18:16:02 +0200

    fix: Delete unnused indexes file

--- main.py
+++ main.py
@@ -2,32 +2,29 @@ from motor.motor_asyncio import AsyncIOMotorClient
 from apscheduler.schedulers.background import BackgroundScheduler
 from asyncclick import command, argument, option, group
 from asyncio import run
-from src import clone_cves, clone_cpe_matchs, clone_cpes, nvd_sync, create_indexes
+from src import clone_cves, clone_cpe_matchs, clone_cpes, nvd_sync
 
 import time
 
 
 @command()
-@argument("mongodb_uri")
-@option("--nvd_api", default="")
+@argument('mongodb_uri')
+@option('--nvd_api', default='')
 async def clone(mongodb_uri, nvd_api):
-    begin = time.time()
-    headers = {"apiKey": nvd_api}
+    headers = {'apiKey': nvd_api}
 
     client = AsyncIOMotorClient(mongodb_uri)
 
     dbs = await client.list_databases()
     while dbs.alive:
         db = await dbs.next()
-        if db["name"] == "nvd":
-            raise Exception("Database have been ready cloned. Delete it or run updater command.")
+        if db['name'] == 'nvd':
+            raise Exception('Database have been ready cloned. Delete it or run updater command.')
 
     if nvd_api:
         delay = 1
     else:
         delay = 6
-
-    await create_indexes(client)
 
     await clone_cves(client, delay, headers)
 
@@ -35,14 +32,12 @@ async def clone(mongodb_uri, nvd_api):
 
     await clone_cpes(client, delay, headers)
 
-    print('Tiempo tardado en clonar la base de datos de NVD: ' + str(time.time() - begin))
-
 
 @command()
-@argument("mongodb_uri")
-@option("--nvd_api", default="")
+@argument('mongodb_uri')
+@option('--nvd_api', default='')
 async def sync(mongodb_uri, nvd_api):
-    headers = {"apiKey": nvd_api}
+    headers = {'apiKey': nvd_api}
 
     client = AsyncIOMotorClient(mongodb_uri)
 
@@ -52,7 +47,7 @@ async def sync(mongodb_uri, nvd_api):
         delay = 6.0
 
     scheduler = BackgroundScheduler()
-    scheduler.add_job(nvd_sync, "interval", args=[client, headers, delay], seconds=7200)
+    scheduler.add_job(nvd_sync, 'interval', args=[client, headers, delay], seconds=7200)
     scheduler.start()
 
 
@@ -64,5 +59,5 @@ async def cli():
 cli.add_command(clone)
 cli.add_command(sync)
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     run(cli())
--- src/__init__.py
+++ src/__init__.py
@@ -2,7 +2,6 @@ from .cve import clone_cves
 from .cpe_match import clone_cpe_matchs
 from .cpe import clone_cpes
 from .updater import nvd_sync
-from .indexes import create_indexes
 
 
-__all__ = ['clone_cves', 'clone_cpe_matchs', 'clone_cpes', 'nvd_sync', 'create_indexes']
+__all__ = ['clone_cves', 'clone_cpe_matchs', 'clone_cpes', 'nvd_sync']
